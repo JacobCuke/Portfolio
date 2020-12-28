678b035fad2fc1e9feeb688e72462fa8a3e958ea
jacob.cuke <jacob.cuke@cs.ucalgary.ca>
2020-12-28 12:47:32 -0700

    Move JSON data into Javascript file

--- javascript/langdata.js
+++ javascript/langdata.js
@@ -1,4 +1,4 @@
-{
+let langdata = {
     "languages": {
         "en": {
             "strings": {
---
+++ javascript/multilang.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let lang = document.documentElement.getAttribute('lang');
+    setText(lang);
+
+    console.log(langdata);
+});
+
+function setText(language) {
+
+}
