a25a4a3f5b438fac979c0323533d3ede6a21f180
Jacob Cuke <jacobcuke@gmail.com>
2020-12-29 11:54:25 -0700

    Add English text to each section

--- javascript/langdata.js
+++ javascript/langdata.js
@@ -6,14 +6,14 @@ let langdata = {
                 "projects-head": "Projects",
                 "japanese-head": "Japanese",
                 "guitar-head": "Guitar",
-                "contact-head": "Contact",
+                "contact-head": "Resume",
                 "lang-selector": "日本語",
                 "main-desc": "Programmer/Guitarist",
-                "about-text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque mauris nunc, eu maximus lacus convallis nec. Donec dictum est vel erat scelerisque, dignissim scelerisque augue aliquet. Donec quis purus blandit, bibendum arcu ac, venenatis elit.",
-                "projects-text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque mauris nunc, eu maximus lacus convallis nec. Donec dictum est vel erat scelerisque, dignissim scelerisque augue aliquet. Donec quis purus blandit, bibendum arcu ac, venenatis elit.",
-                "japanese-text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque mauris nunc, eu maximus lacus convallis nec. Donec dictum est vel erat scelerisque, dignissim scelerisque augue aliquet. Donec quis purus blandit, bibendum arcu ac, venenatis elit.",
-                "guitar-text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque mauris nunc, eu maximus lacus convallis nec. Donec dictum est vel erat scelerisque, dignissim scelerisque augue aliquet. Donec quis purus blandit, bibendum arcu ac, venenatis elit.",
-                "contact-text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce scelerisque mauris nunc, eu maximus lacus convallis nec. Donec dictum est vel erat scelerisque, dignissim scelerisque augue aliquet. Donec quis purus blandit, bibendum arcu ac, venenatis elit."
+                "about-text": "Hello! My name is Jacob Cuke and I'm currently in my final year of Computer Science at the University of Calgary. I have a passion for problem solving and building applications, and have worked on numerous personal projects in my spare time. I also enjoy teaching myself new skills, and have taught myself to speak Japanese fluently, and to play the electric guitar.",
+                "projects-text": "Below are some of the projects that I'm most proud of, and I feel best represent my abilities as a programmer.",
+                "japanese-text": "What started as just a hobby quickly turned into one of my greatest passions as I spent 4 years teaching myself Japanese to fluency. I've worked as an interpretor, given speeches and even taken courses at the university level in Japan.",
+                "guitar-text": "I also taught myself to play the electric guitar, and currently play for one of the largest churches in Canada. Below are some of the things I've worked on and a link to my Instagram where I post guitar covers regularly.",
+                "contact-text": "Below is a link to my full resume and my contact information. Feel free to reach out to me in whatever way works best."
             }
         },
         "jp": {
@@ -22,7 +22,7 @@ let langdata = {
                 "projects-head": "制作",
                 "japanese-head": "日本語",
                 "guitar-head": "ギター",
-                "contact-head": "連絡",
+                "contact-head": "履歴書",
                 "lang-selector": "English",
                 "main-desc": "プログラマー／ギタリスト",
                 "about-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
