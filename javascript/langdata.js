ccd79f4c746e2856a95ca3954d9e76938faa1f70
Jacob Cuke <jacobcuke@gmail.com>
2020-12-29 14:26:11 -0700

    Add embedded videos to guitar section

--- javascript/langdata.js
+++ javascript/langdata.js
@@ -9,27 +9,29 @@ let langdata = {
                 "contact-head": "Resume",
                 "lang-selector": "日本語",
                 "main-desc": "Programmer/Guitarist",
-                "about-text": "Hello! My name is Jacob Cuke and I'm currently in my final year of Computer Science at the University of Calgary. I have a passion for problem solving and building applications, and have worked on numerous personal projects in my spare time. I also enjoy teaching myself new skills, and have taught myself to speak Japanese fluently, and to play the electric guitar.",
+                "about-text": "Hello! My name is Jacob Cuke and I'm currently in my final year of Computer Science at the University of Calgary. I have a passion for problem solving and building applications, and enjoy working on personal projects in my spare time. I also love teaching myself new skills, and have taught myself to speak Japanese fluently, and to play the electric guitar.",
                 "projects-text": "Below are some of the projects that I'm most proud of, and I feel best represent my abilities as a programmer.",
                 "japanese-text": "What started as just a hobby quickly turned into one of my greatest passions as I spent 4 years teaching myself Japanese to fluency. I've worked as an interpretor, given speeches and even taken courses at the university level in Japan.",
                 "guitar-text": "I also taught myself to play the electric guitar, and currently play for one of the largest churches in Canada. Below are some of the things I've worked on and a link to my Instagram where I post guitar covers regularly.",
-                "contact-text": "Below is a link to my full resume and my contact information. Feel free to reach out to me in whatever way works best."
+                "contact-text": "Below is a link to my full resume and my contact information. Feel free to reach out to me in whatever way works best.",
+                "instagram-pic": "Instagram"
             }
         },
         "jp": {
             "strings": {
                 "about-head": "自己紹介",
-                "projects-head": "制作",
+                "projects-head": "制作実績",
                 "japanese-head": "日本語",
                 "guitar-head": "ギター",
                 "contact-head": "履歴書",
                 "lang-selector": "English",
                 "main-desc": "プログラマー／ギタリスト",
-                "about-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
-                "projects-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
-                "japanese-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
+                "about-text": "初めまして！カナダのカルガリー大学コンピューターサイエンス学科の４年生のジェイコブと申します。難題を解決しながらアプリケーションを作成するのがパッションで、卒業後にソフトウェア開発者として日本への移住を目指しています。独学で日本語やギターを習得し、とにかく新しい技術を身に付けることが人生の大きな楽しみとなっています。",
+                "projects-text": "制作の中からもっとも私のプログラマーとしての技量を代表するのに相応しいと思ったプロジェクトを三つ紹介します。",
+                "japanese-text": "趣味で始めたことがここまで大切で大好きなことになってくるとは４年前に思いもしませんでした。暇があればひたすら勉強しづつけ、通訳者の仕事、日本語でのスピーチにプレゼンテーション、日本の大学での正規授業といった経験を通してついに生活や仕事に支障と不自由がないぐらいここまで成長しました。",
                 "guitar-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
-                "contact-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。"
+                "contact-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
+                "instagram-pic": "インスタ"
             }
         }
     }
