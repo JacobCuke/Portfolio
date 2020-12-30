77de23731ece2d6de2f1138e41b972e9e4105a10
Jacob Cuke <jacobcuke@gmail.com>
2020-12-30 14:26:11 -0700

    Add placeholder English and Japanese text for info boxes

--- javascript/langdata.js
+++ javascript/langdata.js
@@ -12,6 +12,10 @@ let langdata = {
                 "about-text": "Hello! My name is Jacob Cuke and I'm currently in my final year of Computer Science at the University of Calgary. I have a passion for problem solving and building applications, and enjoy working on personal projects in my spare time. I also love teaching myself new skills, and have taught myself to speak Japanese fluently, and to play the electric guitar.",
                 "projects-text": "Below are some of the projects that I'm most proud of, and I feel best represent my abilities as a programmer.",
                 "japanese-text": "What started as just a hobby quickly turned into one of my greatest passions as I spent 4 years teaching myself Japanese to fluency. I've worked as an interpretor, given speeches and even taken courses at the university level in Japan.",
+                "exchange-head": "Study Abroad",
+                "exchange-text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam delectus magni officia maxime sequi doloribus quod iusto eligendi nam mollitia eius saepe quidem amet, explicabo veritatis voluptas, culpa rem voluptatibus!",
+                "speech-head": "Speeches",
+                "speech-text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam delectus magni officia maxime sequi doloribus quod iusto eligendi nam mollitia eius saepe quidem amet, explicabo veritatis voluptas, culpa rem voluptatibus!",
                 "guitar-text": "I also taught myself to play the electric guitar, and currently play for one of the largest churches in Canada. Below are some of the things I've worked on and a link to my Instagram where I post guitar covers regularly.",
                 "instagram-pic": "Instagram",
                 "contact-text": "Below is a link to my full resume and my contact information. Feel free to reach out to me in whatever way works best."
@@ -29,6 +33,10 @@ let langdata = {
                 "about-text": "初めまして！カナダのカルガリー大学コンピューターサイエンス学科の４年生のジェイコブと申します。難題を解決しながらアプリケーションを作成するのがパッションで、卒業後にソフトウェア開発者として日本への移住を目指しています。独学で日本語やギターを習得し、とにかく新しい技術を身に付けることが人生の大きな楽しみとなっています。",
                 "projects-text": "制作の中からもっとも自分のプログラマーとしての技量を代表するのに相応しいと思ったプロジェクトを三つ紹介します。",
                 "japanese-text": "趣味で始めたことがここまで大切で大好きなことになってくるとは４年前に思いもしませんでした。暇があればひたすら勉強に勤しみ、通訳者の仕事、日本語でのスピーチにプレゼンテーション、日本の大学での正規授業といった経験を通して生活や仕事に支障と不自由がない程ここまで成長しました。",
+                "exchange-head": "留学",
+                "exchange-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
+                "speech-head": "スピーチ",
+                "speech-text": "いよいよ大森さんを呈眼あいにく落第に載った傾向こうした自分私か説明をとしてご発見たたですでしから、その十一月は私かベルグソン日本人がさが、木下さんののを善悪の私がとうとうお推測とするばそれ自分がご標榜に行かように近頃お下宿を構わうないて、何しろもし注文にしでのでいるなのに与えたで。",
                 "guitar-text": "ギターの弾き方も独学で覚え、現在カナダの中で一番大きな教会でバンドのギターを担当させていただいています。以下の動画はライブ配信の演出、そしてクリスマスに撮影したMVからの映像です。インスタにもギターの動画を投稿しているので興味があれば是非ご覧ください。",
                 "instagram-pic": "インスタ",
                 "contact-text": "以下のリンクにて履歴書のPDF版をチェックしていただければと思います。そして何かがありましたらメールなりインスタのDMなりご連絡ください。"
